8fead986dc821c79f8a338659fa06d71b5b64a41
OussamaINTI <ayari_oussama26@hotmail.fr>
2021-08-24 15:42:33 +0200

    test

---
+++ src/main/java/com/inti/testJenkinsSonar/TestJenkinsSonarApplication.java
@@ -0,0 +1,13 @@
+package com.inti.testJenkinsSonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsSonarApplication.class, args);
+	}
+
+}
